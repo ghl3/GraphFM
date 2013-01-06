5e4033f59d51c38989cc95d7eaf369737a71578d
George Lewis <ghl227@nyu.edu>
2013-01-06 15:43:54 -0500

    Slightly refactored graph class

--- static/graph2.js
+++ static/graph2.js
@@ -2,26 +2,26 @@
 // Based on:
 // http://stackoverflow.com/questions/9539294/adding-new-nodes-to-force-directed-layout
 
-var color = d3.scale.category20();
-
-function myGraph(el) {
+function myGraph(svg) {
 
     var self = this;
 
+    var color = d3.scale.category20();
+
     // Add and remove elements on the graph object
-    this.addNode = function (id) {
-	nodes.push({"id":id});
+    this.addNode = function(name, group) {
+	nodes.push({"name":name, "group":group});
 	update();
     }
 
-    this.removeNode = function (id) {
+    this.removeNode = function(name) {
 	var i = 0;
-	var n = findNode(id);
+	var n = findNode(name);
 	while (i < links.length) {
 	    if ((links[i]['source'] == n)||(links[i]['target'] == n)) links.splice(i,1);
 	    else i++;
 	}
-	nodes.splice(findNodeIndex(id),1);
+	nodes.splice(findNodeIndex(name),1);
 	update();
     }
 
@@ -30,21 +30,13 @@ function myGraph(el) {
 	update();
     }
 
-    var findNode = function(id) {
-	for (var i in nodes) {if (nodes[i]["id"] === id) return nodes[i]};
+    var findNode = function(name) {
+	for (var i in nodes) {if (nodes[i]["name"] === name) return nodes[i]};
     }
     
-    var findNodeIndex = function(id) {
-        for (var i in nodes) {if (nodes[i]["id"] === id) return i};
+    var findNodeIndex = function(name) {
+        for (var i in nodes) {if (nodes[i]["name"] === name) return i};
     }
-
-    // set up the D3 visualisation in the specified element
-    var w = 960; //$(el).innerWidth(),
-    var h = 500; //$(el).innerHeight();
-
-    var vis = this.vis = d3.select(el).append("svg:svg")
-        .attr("width", w)
-        .attr("height", h);
 
     var force = d3.layout.force()
         .gravity(.05)
@@ -56,7 +48,7 @@ function myGraph(el) {
     links = force.links();
 
 
-    var makeid = function() {
+    var makename = function() {
 	var text = "";
 	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	
@@ -66,11 +58,30 @@ function myGraph(el) {
 	return text;
     }
 
+    var begin = function() {
+	console.log("Begin");
+
+	// Get the JSON
+	d3.json('/static/miserables.json', function(error, json) {
+
+	    for(var i=0; i < json.nodes.length; ++i) {
+		nodes.push(json.nodes[i]);
+	    }
+	    for(var i=0; i < json.links.length; ++i) {
+		links.push(json.links[i]);
+	    }
+
+	    // Now, update
+	    update();
+	});
+
+    }
 
     var update = function () {
+	console.log("Update");
 
         var link = vis.selectAll("line.link")
-            .data(links, function(d) { return d.source.id + "-" + d.target.id; });
+            .data(links, function(d) { return d.source.name + "-" + d.target.name; });
 
         link.enter().insert("line")
             .attr("class", "link");
@@ -78,7 +89,7 @@ function myGraph(el) {
         link.exit().remove();
 
         var node = vis.selectAll("g.node")
-            .data(nodes, function(d) { return d.id;});
+            .data(nodes, function(d) { return d.name;});
 
         var nodeEnter = node.enter().append("g")
             .attr("class", "node")
@@ -88,20 +99,27 @@ function myGraph(el) {
             .attr("class", "circle")
             //.attr("xlink:href", "https://d3nwyuy0nl342s.cloudfront.net/images/icons/public.png")
             .attr("r", 10)
-            .style("fill", function(d) { return color(Math.floor(Math.random()*21)); })
+            .style("fill", function(d) { return color(d.group); })
+	    .append("svg:title")
+	    .text(function(d) { return d.name; });
+//            .style("fill", function(d) { return color(Math.floor(Math.random()*21)); })
 
+	
+	/*
         nodeEnter.append("text")
             .attr("class", "nodetext")
             .attr("dx", 12)
             .attr("dy", ".35em")
-            .text(function(d) {return d.id});
-
+            .text(function(d) {return d.name});
+	*/
+	
 	// Make the ability to add new nodes
 	// by clicking
 	nodeEnter.on("click", function(d) {
-	    var id = makeid();
-	    self.addNode(id);
-	    self.addLink(d.id, id);
+	    var name = makename();
+	    var group = d.group;
+	    self.addNode(name, group);
+	    self.addLink(d.name, name);
 	    /*
 	    console.log(d.name)
 	    var new_name = d.name + "_Bob";
@@ -114,6 +132,9 @@ function myGraph(el) {
 	    */
 	});
 	
+
+	//nodeEnter.on("mouseover", addLabel);
+	//nodeEnter.on("mouseout", clearLabel);
 
         node.exit().remove();
 
@@ -132,15 +153,30 @@ function myGraph(el) {
     }
 
     // Make it all go
-    update();
+    begin();
+    //update();
 }
 
-graph = new myGraph("body");
+
+// set up the D3 visualisation in the specified element
+var w = 960; //$(el).innerWidth(),
+var h = 700; //$(el).innerHeight();
+
+var svg = this.vis = d3.select("body").append("svg:svg")
+    .attr("width", w)
+    .attr("height", h);
+
+
+graph = new myGraph(svg);
+//graph = new myGraph("body");
+// graph.begin();
 
 // You can do this from the console as much as you like...
+/*
 graph.addNode("Cause");
 graph.addNode("Effect");
 graph.addLink("Cause", "Effect");
 graph.addNode("A");
 graph.addNode("B");
 graph.addLink("A", "B");
+*/
