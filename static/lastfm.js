ba1c62c58df6e19612f2971e4e16ae95715c5c79
George Lewis <ghl227@nyu.edu>
2013-01-07 19:41:51 -0500

    Now, one needs to click on nodes to select them, and they stay selected

--- static/lastfm.js
+++ static/lastfm.js
@@ -45,8 +45,8 @@ myGraph.prototype.on_click = function(node) {
     else if( click_action == "neighbor" ) {
 	this.add_lastfm_neighbor(node);
     }
-    else if( click_action == "top_artists" ) {
-	show_top_artists(node);
+    else if( click_action == "select_node" ) {
+	select_node(node);
     }
     else {
 	console.log("Error: Unexpected click action: " + click_action);
@@ -62,13 +62,11 @@ myGraph.prototype.add_lastfm_neighbor = function(node) {
     var neigh_url = lastfm.user_getneighbours({"user" : node.name});
     d3.json(neigh_url, function(json) {
 
-	// Pick a random neighbor to add
-	// To ADD: If user already exists,
-	// pick again
+	// Add the nearest neighbor that hasn't
+	// already been added
+	var neighbour = null;
 	var neighbours = json.neighbours.user;
 	neighbours.sort(function(a,b) { return a.match - b.match;}); //neighbor
-
-	var neighbour = null;
 	for(var idx=0; idx < neighbours.length; ++idx) {
 	    if(self.findNode(neighbours[idx].name) == null){
 		neighbour = neighbours[idx];
@@ -81,13 +79,11 @@ myGraph.prototype.add_lastfm_neighbor = function(node) {
 	console.log("Adding Neighbor: ");
 	console.log(neighbour);
 
-	//var len = neighbours.user.length;
-	//var idx = Math.floor(Math.random()*len);
-	//var neighbour = neighbours[idx];
-
+	// Now that we have the neighbor to add,
+	// determine the link length and get
+	// more info about that neighbor
 	var link_length = Math.max(Math.ceil((neighbour.match-.99)*1000), 1.0);
 	var neighbour_url = lastfm.user_getinfo({"user" : neighbour.name});
-	
 	d3.json(neighbour_url, function(error, json) {
 	    if( json.error != null ) {
 		console.log("No User Found with name: " + neighbour.name);
@@ -96,30 +92,21 @@ myGraph.prototype.add_lastfm_neighbor = function(node) {
 	    var neighbour_user = json.user;
 	    neighbour_user['group'] = node.group;
 	    self.addNeighbor(node, neighbour_user, link_length);
+	    select_node(neighbour_user);
 	})
-	
-	//self.addNeighbor(node, neighbor, link_length);
-	
     });
 }
 
 myGraph.prototype.on_mouseover = function(node) {
-    $("#selected_user_name").text(node.name);
-    $("#selected_real_name").text(node.realname);
-    $("#selected_age").text(node.age);
-    $("#selected_gender").text(node.gender);
 }
 
 myGraph.prototype.on_mouseout = function(node) {
-    $("#selected_user_name").text('');
-    $("#selected_real_name").text('');
-    $("#selected_age").text('');
-    $("#selected_gender").text('');
 }
 
 
 var reset = function() {
     graph.reset();
+    deselect_node();
 }
 
 var add_user_node = function() {
@@ -135,39 +122,41 @@ var add_user_node = function() {
 	}
 	var neighbour_user = json.user;
 	graph.addNode(json.user);
+	select_node(json.user);
     })
 }
 
+var select_node = function(node) {
 
-var show_top_artists = function(node) {
+    // Display the user's info
+    $("#selected_user_name").text(node.name);
+    $("#selected_real_name").text(node.realname);
+    $("#selected_age").text(node.age);
+    $("#selected_gender").text(node.gender);
+
     // Display the top artists of the
     // user associated with the given node
-    
     var top_url = lastfm.user_gettopartists({"user" : node.name, "limit" : 5});
     d3.json(top_url, function(json) {
-	console.log(json);
 	var artist_array = json.topartists.artist;
-	console.log(artist_array);
-
-	// Create a table in the dom, 
-	// add the artist list,
-	// and then show the table
 	$('#top_artists').empty();
 	var table = $('<table style="text-align: center;"></table>').addClass('control_panel');
-	table.append("<tr><td class='label'>Top Artists</td></tr>");
+	table.append('<tr><td class="label" style="text-align: center;" >Top Artists</td></tr>');
 	for(var i=0; i<5; i++){
 	    var row = $('<tr></tr>').addClass('bar').text(artist_array[i].name);
 	    table.append(row);
 	}
 	$('#top_artists').append(table);
-
-	//for(var idx=0; idx < artist_array.length; ++idx) {
-	//    console.log(artist_array[idx].name);
-	//}
-	
     })
-    
 } 
+
+var deselect_node = function() {
+    $("#selected_user_name").text('');
+    $("#selected_real_name").text('');
+    $("#selected_age").text('');
+    $("#selected_gender").text('');
+    $('#top_artists').empty();
+}
 
 $(document).ready(function() {
 
@@ -175,7 +164,9 @@ $(document).ready(function() {
 	.attr("width", 960)
 	.attr("height", 700);
 
+    graph = new myGraph(svg, new Array(), new Array());
     // Initialize the graph with the selected user
+    /*
     var user_url = lastfm.user_getinfo({"user" : "rj"});
     d3.json(user_url, function(error, json) {
 	var user = json.user;
@@ -184,5 +175,5 @@ $(document).ready(function() {
 	graph = new myGraph(svg, new Array(user), new Array());
 	console.log("Successfully made graph");
     })
-
+    */
 });
